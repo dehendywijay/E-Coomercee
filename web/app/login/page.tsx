42acf0fc658940269eb861e822e24814dc5c5e2a
DehendyWijaya <dehendywijaya3008@gmail.com>
2025-11-23 06:52:21 +0700

    pembuatan form login

---
+++ web/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { CardDemo } from '@/components/login-form'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gray-200">
+      <div className="w-full max-w-sm">
+      <CardDemo/>
+      </div>
+    </div>
+  )
+}
--- web/components/login-form.tsx
+++ web/components/login-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardAction,
@@ -8,10 +8,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Button } from "./ui/button"
 
 export function CardDemo() {
   return (
