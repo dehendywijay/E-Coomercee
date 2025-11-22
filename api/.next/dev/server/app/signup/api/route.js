0e562e65f9b6cc05b45ce749602033f529059170
DehendyWijaya <dehendywijaya3008@gmail.com>
2025-11-23 02:14:31 +0700

    first commit

---
+++ api/.next/dev/server/app/signup/api/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/signup/api/route.js")
+R.c("server/chunks/node_modules_next_649d2779._.js")
+R.c("server/chunks/[root-of-the-server]__bde187dc._.js")
+R.c("server/chunks/_next-internal_server_app_signup_api_route_actions_07116855.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/signup/api/route.tsx [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/signup/api/route.tsx [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
