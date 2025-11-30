d96344528dbe794977d65838cb5a56b8088f3986
DehendyWijaya <dehendywijaya3008@gmail.com>
2025-11-30 20:28:58 +0700

    penambahan carousel menggunakan embla

--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <section className="flex min-h-svh w-full items-top justify-center p-6 md:p-10 bg-gray-200">
+    <section >
       <HomeForm/>
     </section>
   )
--- web/components/dashboard-form.tsx
+++ web/components/dashboard-form.tsx
@@ -2,57 +2,81 @@
 
 import React from 'react'
 import { Input } from './ui/input'
-import { AlarmCheckIcon, Bell, Mail, Menu, Search, ShoppingCart } from 'lucide-react';
+import { AlarmCheckIcon, Bell, Mail, Menu, Search, ShoppingCart, User } from 'lucide-react';
 import { Button } from './ui/button';
+import useEmblaCarousel from 'embla-carousel-react';
+import { EmblaCarousel } from './ui/embla';
+
+
 export function HomeForm(){
+
+
   return (
-    <section className="flex items-top space-x-2 w-full max-w-lg" >
-      <Button 
-        variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
-        size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
-        aria-label="Keranjang Belanja"
-        
-      >
-        <Menu className="h-5 w-5" />
-      </Button>
-
-        <section className="relative flex-grow ">
-          <Search className="absolute left-3 top-4.25 h-4 w-4 -translate-y-1/2 text-muted-foreground"/>
-          <Input className="pl-9 w-full"
-          type="search" 
-          placeholder="search" 
-          />
-        </section>
-        <section className="flex ml-7">
-          <Button 
-            variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
-            size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
-            aria-label="Keranjang Belanja"
-            
-          >
-            <ShoppingCart className="h-5 w-5 " />
-          </Button>
-
-          <Button 
-            variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
-            size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
-            aria-label="Keranjang Belanja"
-            
-          >
-            <Bell className="h-5 w-5" />
-          </Button>
-
-          <Button 
-            variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
-            size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
-            aria-label="Keranjang Belanja"
-            
-          >
-            <Mail className="h-5 w-5" />
-          </Button>
+    <section className="flex flex-col min-h-svh items-center w-full p-6 md:p-10 bg-gray-200">
+      <section className="flex space-x-2 w-full max-w-lg" >
+        <Button 
+          variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
+          size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
+          aria-label="Keranjang Belanja"
           
-        </section>
+        >
+          <Menu className="h-5 w-5" />
+        </Button>
 
+          <section className="relative flex-grow ">
+            <Search className="absolute left-3 top-4.25 h-4 w-4 -translate-y-1/2 text-muted-foreground"/>
+            <Input className="pl-9 w-full"
+            type="search" 
+            placeholder="search" 
+            />
+          </section>
+          <section className="flex ml-7">
+            <Button 
+              variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
+              size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
+              aria-label="Keranjang Belanja"
+              
+            >
+              <ShoppingCart className="h-5 w-5 " />
+            </Button>
+
+            <Button 
+              variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
+              size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
+              aria-label="Keranjang Belanja"
+              
+            >
+              <Bell className="h-5 w-5" />
+            </Button>
+
+            <Button 
+              variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
+              size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
+              aria-label="Keranjang Belanja"
+              
+            >
+              <Mail className="h-5 w-5" />
+            </Button>
+            
+          </section>
+
+          <section className="flex ml-7">
+            <Button 
+              variant="ghost" // Menggunakan varian 'outline' agar terlihat seperti tombol UI
+              size="icon-lg"     // Menggunakan ukuran 'icon' agar tombol menjadi persegi kecil
+              aria-label="Keranjang Belanja"
+              
+            >
+            <User className="h-5 w-5" />
+            </Button>
+          </section>
+      </section>
+      <section className='flex flex-col mt-10' >
+        <EmblaCarousel/>
+      </section>
+      <section>
+        
+      </section>
     </section>
   )
 }
