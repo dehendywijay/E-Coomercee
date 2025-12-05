50fcf11c24cb83c00d75a0dd1a3266a2521dd265
DehendyWijaya <dehendywijaya3008@gmail.com>
2025-12-06 02:28:53 +0700

    penambahanan confirm password saat register

--- api/app/api/signup/route.tsx
+++ api/app/api/signup/route.tsx
@@ -1,16 +1,21 @@
 
 import { PrismaClient } from "@/app/generated/prisma/client";
 import bcrypt from "bcryptjs";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
     const data = await req.json();
     const nama = data.name;
     const password = data.password;
+    const confirmPassword = data.confirmPassword;
     const email = data.email;
 
+    if (password !== confirmPassword) {
+        return NextResponse.json(
+        { message: "Data Tidak berhasil disimpan(Password dan Confirm Password tidak sesuai)" })
+    }
     const check = await prisma.user.findFirst({
         where : {
             email : email
@@ -24,7 +29,7 @@ export async function POST(req: Request) {
         return NextResponse.json(
         { message: "Data Tidak berhasil disimpan(Email Sudah Terdaftar)", status: false })
     }
-    const salt = await bcrypt.genSalt(10); // Level kerumitan hashing (10 adalah standar yang baik)
+    const salt = await bcrypt.genSalt(10); 
     const hashedPassword = await bcrypt.hash(password, salt);
 
     await prisma.user.create({
