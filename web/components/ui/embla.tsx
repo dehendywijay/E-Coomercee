0045faea88084c0f76b880e6cf56cdfff87035bf
DehendyWijaya <dehendywijaya3008@gmail.com>
2025-12-05 08:38:41 +0700

    menambahkan gambar pada carousel

--- web/components/ui/embla.tsx
+++ web/components/ui/embla.tsx
@@ -1,16 +1,38 @@
-import React from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
+import Image from 'next/image'
 
 export function EmblaCarousel() {
   const [emblaRef] = useEmblaCarousel()
 
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
-      </div>
-    </div>
+    <section className="embla" ref={emblaRef}>
+      <section className="embla__container  ">
+        <section className="embla__slide relative w-full h-64 " >
+          <Image 
+            src="/images/TP1.jpg" 
+            alt="  " 
+            fill
+            className="rounded-xl object-cover"
+          />
+        </section>
+        <section className="embla__slide relative w-full h-64">
+          <Image 
+            src="/images/TP2.jpg" 
+            alt=" " 
+            fill
+            className="rounded-xl object-cover"
+          />
+        </section>
+        <section className="embla__slide relative w-full h-64" >
+          <Image
+            src="/images/TP3.jpg" 
+            alt=" " 
+            width={800}
+            height={800}
+            className="rounded-xl object-cover"
+          />
+        </section>
+      </section>
+    </section>
   )
 }
