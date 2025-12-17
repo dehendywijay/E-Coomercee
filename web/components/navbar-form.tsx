4ffa5497f4fb4617af57d5768d2c60f3289d20c8
DehendyWijaya <dehendywijaya3008@gmail.com>
2025-12-17 15:29:49 +0700

    membuat agar bisa menarik data melalui refreshToken

--- web/components/navbar-form.tsx
+++ web/components/navbar-form.tsx
@@ -3,11 +3,47 @@
 import { Input } from './ui/input'
 import {  Bell, Mail, Menu, Search, ShoppingCart, User } from 'lucide-react';
 import { Button } from './ui/button';
-
+import { useState, useEffect } from "react"
+import axios from 'axios';
 
 
 export function NavbarDefault(){
- 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [nama, setNama] = useState("");
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [token, setToken] = useState("");
+
+      const decodeJwtPayload = (token: string) => {
+      if (!token) {
+        throw new Error("Token kosong");
+      }
+      const base64 = token.split(".")[1]
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+      return JSON.parse(atob(base64));
+    };
+  const refreshToken = async () => {
+    console.log("refreshToken jalan");
+    try {
+      const response = await axios.get("http://localhost:3001/api/auth/refresh",{
+        withCredentials: true,
+      });
+      
+  const accessToken = response.data.accesToken;
+  setToken(accessToken);
+  const decoded = decodeJwtPayload(accessToken);
+  setNama(decoded.name);
+  console.log("decoded payload:", decoded.name);
+  
+    } catch (error) {
+        console.error("error refreshToken:", error);
+    }
+  }
+   // eslint-disable-next-line react-hooks/rules-of-hooks
+   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    refreshToken();
+  }, []);
   
   return (
     <section className="flex flex-col min-h-svh items-center w-full p-6 md:p-10 bg-gray-200">
@@ -68,7 +104,7 @@ export function NavbarDefault(){
               
             >
             <User className="h-5 w-5" />
-            
+            <p>{nama}</p>
             </Button>
           </section>
       </section>
