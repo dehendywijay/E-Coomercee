af58b82ea01afae88496ac3f47de3f667af365bc
DehendyWijaya <dehendywijaya3008@gmail.com>
2025-12-17 10:16:10 +0700

    membuat template gerenate refreshtoken

--- api/lib/token.ts
+++ api/lib/token.ts
@@ -14,3 +14,16 @@ export async function createJoseToken(payload: { id: number; email: string }, ex
         
     return token;
 }
+
+export async function createJoseRefreshToken(payload: { id: number; email: string }, expiresIn: string): Promise<string> {
+ 
+    const secret = new TextEncoder().encode(process.env.REFRESH_TOKEN_SECRET!);
+    
+    const token = await new SignJWT(payload)
+        .setProtectedHeader({ alg: 'HS256' }) 
+        .setIssuedAt()
+        .setExpirationTime(expiresIn) 
+        .sign(secret); 
+        
+    return token;
+}
