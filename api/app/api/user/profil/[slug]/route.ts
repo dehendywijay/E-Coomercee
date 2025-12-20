780be3b86ebc7caf6a8baa7be38838d24efac95f
unknown <dehendywijaya2@gmail.com>
2025-12-20 20:43:20 +0700

    membuat function get yang yang bisa diambil menggunakan id dari user

---
+++ api/app/api/user/profil/[slug]/route.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+export const GET = async (res : NextResponse,  
+    ctx: { params: Promise<{ slug: string }> }) => {
+    const { slug } = await ctx.params;
+    
+    const profil = await prisma.profile.findUnique({
+        where : {
+            userId : Number(slug)
+        },select : {
+            gender : true,
+            phone : true,
+            address : true,
+            birthDate: true
+        }
+    });
+
+    if (!profil) {
+    return NextResponse.json(
+      { message: "Profile tidak ditemukan" },
+      { status: 404 }
+    );
+  }
+    return NextResponse.json(profil);
+    
+}
