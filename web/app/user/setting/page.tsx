a0e607796205ae74be526078f0c38cb2cf616a99
unknown <dehendywijaya2@gmail.com>
2025-12-20 14:11:11 +0700

    membuar route user/setting yang berisi profilPage

---
+++ web/app/user/setting/page.tsx
@@ -0,0 +1,12 @@
+import { ProfilePage } from '@/components/user-form'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <div>
+      <ProfilePage/>
+      </div>
+    </div>
+  )
+}
