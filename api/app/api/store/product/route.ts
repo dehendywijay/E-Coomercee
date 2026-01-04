d8ccf0c6b726b6059a43ef135dda5a23f51d24b1
DehendyWijaya <dehendywijaya3008@gmail.com>
2026-01-04 13:40:41 +0700

    mengambil id user dari cookie

---
+++ api/app/api/store/product/route.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { jwtVerify } from "jose";
+import { NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+
+async function getUserIdFromRequest(req: NextRequest) {
+  const authHeader = req.headers.get("authorization");
+  if (!authHeader) throw new Error("Unauthorized");
+
+  const token = authHeader.split(" ")[1];
+  const { payload } = await jwtVerify(token, secret);
+  return payload.id as number; 
+}
+export async function POST(){
+
+}
