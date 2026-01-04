09b4fe64f541e9f1eaa3e0be764606ff323e5e50
rireeee666 <ridhoresky436@gmail.com>
2026-01-05 04:27:03 +0700

    mencari product menggunakan findFirst yang dimana storeid = slug

--- api/app/api/store/product/[slug]/route.ts
+++ api/app/api/store/product/[slug]/route.ts
@@ -5,6 +5,29 @@ const prisma = new PrismaClient()
 export const GET = async (res : NextResponse,  
     ctx: { params: Promise<{ slug: string }> }) => {
     const { slug } = await ctx.params;
+
+    const product = await prisma.product.findFirst({
+        where : {
+            storeId : Number(slug)
+        },select : {
+            name : true,
+            imageSrc : true,
+            originalPrice : true,
+            discountPercentage : true,
+            rating : true,
+            salesCount : true,
+            location : true,
+            stock : true,
+            description : true,
+        }
+    });
+    if (!product) {
+        return NextResponse.json(
+            { message: "Profile tidak ditemukan" },
+            { status: 404 }
+        );
+    }
     
     
+    return NextResponse.json(product);
 }
