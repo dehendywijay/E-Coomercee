f6d566efae1b3f095a70868923012ef1a250116c
rireeee666 <ridhoresky436@gmail.com>
2026-01-05 03:27:41 +0700

    membuat params slug

--- api/app/api/store/product/[slug]/route.ts
+++ api/app/api/store/product/[slug]/route.ts
@@ -1,1 +1,9 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+export const GET = async (res : NextResponse,  
+    ctx: { params: Promise<{ slug: string }> }) => {
+    
+}
