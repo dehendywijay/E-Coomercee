26c1d06b825a67adcc1ff2ea26face5a08f97c17
rireeee666 <ridhoresky436@gmail.com>
2026-01-05 01:52:04 +0700

    membuat variabel yang menampung hasil pencarian di db

--- api/app/api/store/akun/[slug]/route.ts
+++ api/app/api/store/akun/[slug]/route.ts
@@ -6,5 +6,14 @@ export const GET = async (res : NextResponse,
     ctx: { params: Promise<{ slug: string }> }) => {
     const { slug } = await ctx.params;
     
+    const profil = await prisma.store.findUnique({
+        where : {
+            userId : Number(slug)
+        },select : {
+            name : true,
+            location : true
+        }
+    });
+
     
 }
