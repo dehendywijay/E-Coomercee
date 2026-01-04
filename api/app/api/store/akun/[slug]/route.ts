cd496b5eabc4736acf5f5172f169459562d6e486
rireeee666 <ridhoresky436@gmail.com>
2026-01-05 01:50:34 +0700

    membuat funtion get juga menambahkan params slug

--- api/app/api/store/akun/[slug]/route.ts
+++ api/app/api/store/akun/[slug]/route.ts
@@ -2,3 +2,8 @@ import { PrismaClient } from "@/app/generated/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient()
+export const GET = async (res : NextResponse,  
+    ctx: { params: Promise<{ slug: string }> }) => {
+    
+    
+}
