9e508310d671a74c26c3ea2b2d9fb7abeb070b95
dehendywijaya <dehendywijaya2@gmail.com>
2026-01-05 16:26:39 +0700

    merubah logika mengambil product

--- api/app/api/store/product/[slug]/route.ts
+++ api/app/api/store/product/[slug]/route.ts
@@ -1,14 +1,16 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient()
 export const GET = async (res : NextResponse,  
     ctx: { params: Promise<{ slug: string }> }) => {
     const { slug } = await ctx.params;
 
-    const product = await prisma.product.findMany({
+    const product = await prisma.store.findMany({
         where : {
-            storeId : Number(slug)
+            userId : Number(slug)
+        },select : {
+            products : true
         }
     });
     if (!product) {
