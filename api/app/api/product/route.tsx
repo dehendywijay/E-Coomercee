d1ead8d47e41293a3fa36f688fc8daed9f20d234
rireeee666 <ridhoresky436@gmail.com>
2026-01-07 01:59:49 +0700

    -

--- api/app/api/product/route.tsx
+++ api/app/api/product/route.tsx
@@ -1,3 +1,18 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
+
+export async function GET(req : NextRequest, res : NextResponse){
+    try {
+        const body = await req.json();
+        const { name, imageSrc, originalPrice, discountPercentage, rating, salesCount, location, stock, description } = body;
+    }catch (err) {
+    
+    console.error(err);
+    return NextResponse.json(
+      { status: false, message: "Gagal menyimpan Data"},
+      { status: 400 },
+    );
+}
+}
