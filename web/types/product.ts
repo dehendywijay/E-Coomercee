2077ca17e2fc35b15bdf6694b03f10868bccadd3
rireeee666 <ridhoresky436@gmail.com>
2026-01-08 02:11:01 +0700

    membuat template untuk product

---
+++ web/types/product.ts
@@ -0,0 +1,14 @@
+export type Product = {
+  id: string;
+  name: string;
+  imageSrc: string;
+  stock: number;
+  originalPrice: number;
+  discountPercentage?: number;
+  rating: number;
+  salesCount: string;
+  bonusText: string;
+  location: string;
+  description: string;
+};
+
