934c08bfe9bb5cd8d0b57a99824baabb34a5de92
unknown <dehendywijaya2@gmail.com>
2026-01-13 10:25:54 +0700

    ubah ke folder lib

--- web/lib/strings.ts
+++ web/lib/strings.ts
@@ -1,2 +1,4 @@
-const api = "http://localhost:3001/api";
-export const api_signup = "http://localhost:3001/signup/api";
+export const api = "http://localhost:3001/api";
+export const api_signup = `${api}/auth/signup`;
+export const api_login = `${api}/auth/login`;
+export const api_refresh = `${api}/auth/refresh`;
