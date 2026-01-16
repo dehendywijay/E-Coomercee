2b641f72ebe32d83ac2380bd0ee72ec08d7c3f48
dehendywijay <dehendywijaya2@gmail.com>
2026-01-16 23:14:48 +0700

    feat:penambahan interface

--- web/types/interface.ts
+++ web/types/interface.ts
@@ -1,13 +1,41 @@
-export interface Product {
+export interface Profile  {
   id: string;
-  name: string;
-  imageSrc: string;
-  stock: number;
-  originalPrice: number;
-  discountPercentage?: number;
-  rating: number;
-  salesCount: string;
-  bonusText: string;
-  location: string;
-  description: string;
+  address?: string;
+  phone?: string;
+  gender?: string;
+  birthDate?: string;
 };
+
+export interface Store  {
+  name   :  string  
+  location  : string
+}
+export interface allProducts  {
+  id : string;
+  name            : string
+  imageSrc        : string
+  stock         : number
+  originalPrice    : number
+  discountPercentage : number
+  rating            : number
+  salesCount        : string
+  bonusText         : string
+  location     :    string
+  description : string
+  storeId?: string
+}
+
+export interface Product {
+  id : string;
+  name            : string
+  imageSrc        : string
+  stock         : number
+  originalPrice    : number
+  discountPercentage? : number
+  rating            : number
+  salesCount        : string
+  bonusText         : string
+  location     :    string
+  description : string
+  storeId?: string
+}
