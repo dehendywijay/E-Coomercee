3c07a5d2df1724f1a1abcc984ce936e7d666ba35
dehendywijay <dehendywijaya2@gmail.com>
2026-01-17 18:51:45 +0700

    feat:ambil data melalui useAuth

--- web/components/shopping/cart-form.tsx
+++ web/components/shopping/cart-form.tsx
@@ -2,6 +2,8 @@
 
 import { Product } from "@/types/interface";
 import { NavbarDefault } from "../header/navbar-form";
+import { useEffect, useState } from "react";
+import { useAuth } from "@/app/context/authcontext";
 
 interface CartProduct extends Product  {
   qty: number;
@@ -9,7 +11,25 @@ interface CartProduct extends Product  {
 }
 
 export function CartItem() {
+  const [cartProducts, setCartProducts] = useState<CartProduct[]>([]);
+  const {user} = useAuth();
+  useEffect(() => {
+    const loadData= async () => {
+      const products = Array.isArray(user?.cartProduct)
+      ? user.cartProduct
+      : [];
+      setCartProducts(
+        products.map((p) => ({
+          ...p,
+          qty: 1,
+          selected: true,
+        }))
+      );
+    };
 
+    loadData();
+    console.log("S", cartProducts)
+  });
   return (
     <main>
       <NavbarDefault />
